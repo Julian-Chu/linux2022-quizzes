7c6eae3489cd4e6025cbd03de15989ccbd830c14
Yu-Lang Chu <yulang.chu@gmail.com>
2022-02-22 22:23:54 +0100

    refactor test case with assert

--- quiz1/question2/question2_linux_style.c
+++ quiz1/question2/question2_linux_style.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #define container_of(ptr, type, member)               \
     ({                                                \
@@ -60,22 +61,40 @@ struct list_head *deleteDuplicates_iter(struct list_head *head)
     return head;
 }
 
-struct list_head *create_list(){
+struct list_head *create_list(int *nums, int nums_size){
+    if(nums_size == 0) return NULL;
+
     struct ListNode *head = malloc(sizeof(struct ListNode));
-    head->val = 1;
-    struct ListNode *node1 = malloc(sizeof(struct ListNode));
-    node1->val = 1;
-    (&head->node)->next = &node1->node;
-    struct ListNode *node2 = malloc(sizeof(struct ListNode));
-    node2->val = 2;
-    (&node1->node)->next = &node2->node;
-    struct ListNode *node3 = malloc(sizeof(struct ListNode));
-    node3->val = 2;
-    (&node2->node)->next = &node3->node;
-    struct ListNode *node4 = malloc(sizeof(struct ListNode));
-    node4->val = 2;
-    (&node3->node)->next = &node4->node;
+    head->val = nums[0];
+    struct ListNode *cur, *next;
+    cur = head;
+
+    for(int i =1; i < nums_size;i++){
+        next = malloc(sizeof(struct ListNode));
+        next->val = nums[i];
+        (&cur->node)->next = &next->node;
+        cur = next;
+    }
+
+
     return &head->node;
+}
+
+int list_equal(struct list_head *h1, struct list_head *h2){
+    struct ListNode *Node1, *Node2;
+    while(h1 && h2){
+        Node1 = container_of(h1, struct ListNode, node);
+        Node2 = container_of(h2, struct ListNode, node);
+        if(Node1->val != Node2->val)
+            return 0;
+        h1 = h1->next;
+        h2 = h2->next;
+    } 
+
+    if(!h1 && !h2) 
+        return 1;
+
+    return 0;
 }
 
 void print_ListNodes(struct list_head *head){
@@ -91,14 +110,20 @@ void print_ListNodes(struct list_head *head){
 }
 
 int main(void){
-    struct list_head *head = create_list();
+    struct list_head *expect = create_list((int[]){1,2,3}, 3);
+
+    printf("testing deleteDuplicates_iter\n");
+    struct list_head *head = create_list((int[]){1,2,2,3,3,3}, 6);
     head = deleteDuplicates(head);
-    print_ListNodes(head);
+    assert(list_equal(expect, head));
 
 
-    struct list_head *head_iter = create_list();
+    printf("testing deleteDuplicates\n");
+    struct list_head *head_iter = create_list((int[]){1,2,2,3,3,3}, 6);
     head_iter = deleteDuplicates_iter(head_iter);
-    print_ListNodes(head_iter);
+    assert(list_equal(expect, head_iter));
+
+    printf("all tests passed\n");
 }
 
 
